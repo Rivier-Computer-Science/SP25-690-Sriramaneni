4286f3bd5285bf9bd06b4cfb4d6abae8456aa2b4
akhilgit-glitch <asriramaneni@rivier.edu>
2026-04-18 13:47:11 -0400

    metadata&reqires

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,11 +2,19 @@ import pandas as pd
 
 class DataLoader:
     def __init__(self, csv_path):
+        """
+        Handles loading of metadata CSV file
+        """
         self.csv_path = csv_path
 
     def load_data(self):
         """
-        Load dataset metadata (audio filenames + emotion labels)
+        Loads dataset into pandas dataframe
         """
         df = pd.read_csv(self.csv_path)
+
+        print("Dataset Loaded Successfully!")
+        print("Shape:", df.shape)
+        print(df.head())
+
         return df
